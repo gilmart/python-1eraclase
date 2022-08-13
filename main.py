6fa61e1ca451c0ce771303e7e8e2e0ad3bb8abac
209 <209@academicos.cesde.edu.co>
2022-08-13 12:51:41 -0500

    1era clase

---
+++ main.py
@@ -0,0 +1,37 @@
+# Hola mundo con Python  
+
+# SALIDA POR CONSOLA
+'''
+    print("Hola Mundo, soy Gilma")
+    print('Hola Mundo, soy Gilma')
+'''
+# ENTRADAS DE DATOS: VARIABLES
+# DATOS PRIMITIVOS
+dato = None 
+nombre = 'Gilma'
+edad = 30
+estatura = 1.73
+hincha = True
+
+#SALIDA POR TECLADO
+
+print("hola" ,nombre,"Tu edad es: " ,edad)
+print(f'hola {nombre}, tu edad es {edad}')
+
+#RECIBIR DATOS POR TECLADO
+ciudad = input('Digita la ciudad donde vives: ')
+print(f'la ciudad donde usted vive es: {ciudad}')
+
+#RECIBIR DATOS NUMERICOS POR TECLADO
+numero1 = int(input('Digita un numero: '))
+numero2 = float(input('Digita otro numero: '))
+print(f'El numero 1 es: {numero1} El numero 2 es: {numero2}')
+
+#OPERADORES ARITMETICOS
+#(+ - * / %)
+
+suma = numero1 + numero2
+
+print(f'y la suma es: {suma}')
+
+
---
+++ reto1.py
@@ -0,0 +1,14 @@
+#1.
+numero= int(input('Digite un numero entero:'))
+resultado= numero%5
+
+print(f'El residuo es: {resultado}')
+
+#CONDICIONAL SIMPLE EN PYTHON
+
+if(resultado == 0):
+    print('es multiplo de 3')
+else:
+    print('no es mutiplo de 3')
+print ('fin del programa')
+
